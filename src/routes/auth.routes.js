ae8f1f4ac2723b73464f56e4e43c3df18ffead93
Soumi Samanta <samantasoumi10@gmail.com>
2025-11-20 12:10:18 +0530

    feat: add authentication middleware to logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", logoutUser);
+router.post("/logout", auth, logoutUser);
 router.post("/refresh-token", refreshAccessToken);
 
 export default router;
